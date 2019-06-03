98816ea4209af681a16f2e630a260bb0c249e26d
Andrew Dorokhin <andrew@dorokhin.moscow>
2019-06-03 22:02:00 +0700

    add business logic

--- progress/views.py
+++ progress/views.py
@@ -7,6 +7,8 @@ from django.views.generic.list import ListView
 from progress.forms import PassEntryAddForm
 from django.urls import reverse_lazy
 from django.utils import timezone
+from django.db.models import Count, DateField
+from django.db.models.functions import Trunc
 
 
 class EntryTypeListView(ListView):
@@ -22,15 +24,30 @@ class EntryTypeListView(ListView):
 class EntryTypeDetailView(DetailView):
     model = EntryType
 
+    def get_object(self, queryset=None):
+        pass
+
+    def get_context_data(self, **kwargs):
+        context = super(EntryTypeDetailView, self).get_context_data(**kwargs)
+        context['entries'] = PassEntry.objects.order_by()\
+            .annotate(day=Trunc('creation_date', 'day', output_field=DateField()))\
+            .values('day',).annotate(all_that_day=Count('id'))
+
+        return context
+
 
 class PassEntryCreateView(CreateView):
     model = PassEntry
     form_class = PassEntryAddForm
 
     def get_success_url(self):
-        return reverse_lazy('realty:house-list')
+        return reverse_lazy('progress:entry-detail', kwargs={'pk': self.kwargs.get(self.pk_url_kwarg)})
 
     def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.article = get_object_or_404(EntryTypeListView, id=self.kwargs.get('article_pk'))
+
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.entry_type, _ = EntryType.objects.get_or_create(pk=self.kwargs.get(self.pk_url_kwarg))
+        obj.save()
+
         return super().form_valid(form)
