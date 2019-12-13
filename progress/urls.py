bde447cb1b68476a0d6422ac204e832725c53936
Andrew Dorokhin <andrew@dorokhin.moscow>
2019-12-13 15:36:31 +0700

    add main page view

--- progress/urls.py
+++ progress/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from progress.views import EntryTypeListView, PassEntryCreateView, EntryTypeDetailView
+from progress.views import EntryTypeListView, PassEntryCreateView, EntryTypeDetailView, MainPageView
 
 app_name = 'progress'
 
 urlpatterns = [
-    path('', EntryTypeListView.as_view(), name='entry-list'),
+    path('', MainPageView.as_view(), name='entry-list'),
     path('<int:pk>/detail', EntryTypeDetailView.as_view(), name='entry-detail'),
     path('<int:pk>/create/', PassEntryCreateView.as_view(), name='entry-create'),
 
